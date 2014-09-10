63e424eb0cdde9190e8cf645c46f35ab1e202101
Michael Ferris <mike.ferris@hotmail.com>
2014-09-10 16:04:32 -0400

    fix react-bootstrap avec webpack

--- Koop/js/entry/index.js
+++ Koop/js/entry/index.js
@@ -1,6 +1,9 @@
 /** @jsx React.DOM */
 var mountNode = document.getElementById('content');
 
+var React = require('react');
+var ReactBootstrap = require('react-bootstrap');
+
 var BS = ReactBootstrap;
 
 var buttonsInstance = (
@@ -10,25 +13,4 @@ var buttonsInstance = (
     </BS.ButtonToolbar>
 );
 
-var buttonGroupInstance = (
-    <ButtonToolbar>
-      <ButtonGroup>
-        <Button>1</Button>
-        <Button>2</Button>
-        <Button>3</Button>
-        <Button>4</Button>
-      </ButtonGroup>
-
-      <ButtonGroup>
-        <Button>5</Button>
-        <Button>6</Button>
-        <Button>7</Button>
-      </ButtonGroup>
-
-      <ButtonGroup>
-        <Button>8</Button>
-      </ButtonGroup>
-    </ButtonToolbar>
-  );
-
-React.renderComponent(buttonGroupInstance, mountNode);
+React.renderComponent(buttonsInstance, mountNode);
