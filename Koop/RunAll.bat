8f0ef08fea51c046955d6ab4e6c584c95cc6481e
Cellule <mike.ferris@hotmail.com>
2014-09-19 13:46:31 -0400

    Fix for windows solution

--- Koop/RunAll.bat
+++ Koop/RunAll.bat
@@ -1,5 +1,4 @@
 @echo off
-start npm run-script assets-dev
-set PORT=1337
-start node app
+start npm run-script assets-dev-win
+set PORT=1337 && start node app
 start "My Koop" "http://localhost:8080/"
