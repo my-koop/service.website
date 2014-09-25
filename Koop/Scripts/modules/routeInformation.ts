d39288dceac05db68c949e4cc6fa68799736485b
Cellule <mike.ferris@hotmail.com>
2014-09-25 13:01:27 -0400

    Prettier names for the routes

--- Koop/Scripts/modules/routeInformation.ts
+++ Koop/Scripts/modules/routeInformation.ts
@@ -6,7 +6,7 @@ export class RouteInformation {
     if (path) {
       this.fullPath = "/" + path;
     } else {
-      this.fullPath = "/" + this.name;
+      this.fullPath = "/" + this.name.replace(/\s+/g, '');
     }
     this.relativePath = this.fullPath;
   }
@@ -21,7 +21,7 @@ export class RouteInformationTree {
 
     // relative path is full path before fixup with parent
     if (path === undefined){
-      path = name;
+      path = name.replace(/\s+/g, '');;
     }
     this.relativePath = path;
 
@@ -47,39 +47,34 @@ export class RouteInformationTree {
 }
 
 // static pages
-export var homepage = new RouteInformationTree("homepage");
-  export var aboutUs = new RouteInformationTree("aboutUs", homepage);
-  export var pwdRcv = new RouteInformationTree("passwordRecovery", homepage);
-  export var myaccount = new RouteInformationTree("myaccount", homepage);
-    export var register = new RouteInformationTree("register", myaccount);
-    export var login = new RouteInformationTree("login", myaccount);
-  export var shop = new RouteInformationTree("shop", homepage);
-    export var cart = new RouteInformationTree("cart", shop);
+export var homepage = new RouteInformationTree("Homepage");
+  export var aboutUs = new RouteInformationTree("About Us", homepage);
+  export var pwdRcv = new RouteInformationTree("Password Recovery", homepage);
+  export var myaccount = new RouteInformationTree("My Account", homepage);
+    export var register = new RouteInformationTree("Register", myaccount);
+    export var login = new RouteInformationTree("Login", myaccount);
+  export var shop = new RouteInformationTree("Shop", homepage);
+    export var cart = new RouteInformationTree("Cart", shop);
 
 // dashboard pages
-export var dashboard = new RouteInformationTree("dashboard", homepage);
-  export var options = new RouteInformationTree("options", dashboard);
-  export var stats = new RouteInformationTree("stats", dashboard);
-  export var transaction = new RouteInformationTree("transaction_crud", dashboard);
-    export var transaction_list = new RouteInformationTree("transaction_list", transaction);
-  export var events = new RouteInformationTree("events_crud", dashboard);
-    export var events_list = new RouteInformationTree("events_list", events);
-    export var events_pos = new RouteInformationTree("events_pos", events);
-    export var events_report = new RouteInformationTree("events_report", events);
-    export var events_signup = new RouteInformationTree("events_signup", events);
-  export var items = new RouteInformationTree("items_crud", dashboard);
-    export var items_list = new RouteInformationTree("items_list", items);
-    export var items_itemsbelowthreshold = new RouteInformationTree("items_itemsbelowthreshold", items);
-    export var items_nextorder = new RouteInformationTree("items_nextorder", items);
-  export var mailing = new RouteInformationTree("mailing_crud", dashboard);
-    export var mailing_mailinglists = new RouteInformationTree("mailing_lists", mailing);
-    export var mailing_send = new RouteInformationTree("mailing_send", mailing);
-    export var mailing_subscribe = new RouteInformationTree("mailing_subscribe", mailing);
-  export var members = new RouteInformationTree("members_crud", dashboard);
-    export var members_list = new RouteInformationTree("members_list", members);
-    export var members_permissions = new RouteInformationTree("members_permissions", members);
-    export var volunteer_availability = new RouteInformationTree("volunteer_availability", members);
-    export var volunteer_schedule = new RouteInformationTree("volunteer_schedule", members);
+export var dashboard = new RouteInformationTree("Dashboard", homepage);
+  export var options = new RouteInformationTree("Options", dashboard);
+  export var stats = new RouteInformationTree("Stats", dashboard);
+  export var transaction = new RouteInformationTree("Transactions", dashboard);
+  export var events = new RouteInformationTree("Events", dashboard);
+    export var events_pos = new RouteInformationTree("Event Pos", events);
+    export var events_report = new RouteInformationTree("Events Report", events);
+    export var events_signup = new RouteInformationTree("Events Signup", events);
+  export var items = new RouteInformationTree("Items", dashboard);
+    export var items_itemsbelowthreshold = new RouteInformationTree("Items Below Threshold", items);
+    export var items_nextorder = new RouteInformationTree("Supplier Order", items);
+  export var mailing = new RouteInformationTree("Mailing Lists", dashboard);
+    export var mailing_send = new RouteInformationTree("Mailing Send", mailing);
+    export var mailing_subscribe = new RouteInformationTree("Mailing Subscribe", mailing);
+  export var members = new RouteInformationTree("Members", dashboard);
+    export var members_permissions = new RouteInformationTree("Members Permissions", members);
+    export var volunteer_availability = new RouteInformationTree("Volunteer Availability", members);
+    export var volunteer_schedule = new RouteInformationTree("Volunteer Schedule", members);
 
 
 
--- Koop/components/MyKoop.jsx
+++ Koop/components/MyKoop.jsx
@@ -18,8 +18,8 @@ var MyKoop = React.createClass({
           <DefaultRoute handler={MKHomepage}/>
           <Route name={RouteInfo.aboutUs.name} path={RouteInfo.aboutUs.relativePath} handler={MKPlaceHolder}/>
           <Route name={RouteInfo.pwdRcv.name} path={RouteInfo.pwdRcv.relativePath} handler={MKPlaceHolder}/>
-          <Route name={RouteInfo.myaccount.name} path={RouteInfo.myaccount.relativePath} handler={MKPlaceHolder}/>
-            <Route name={RouteInfo.register.name} path={RouteInfo.register.relativePath} handler={MKPlaceHolder}>
+          <Route name={RouteInfo.myaccount.name} path={RouteInfo.myaccount.relativePath} handler={MKPlaceHolder}>
+            <Route name={RouteInfo.register.name} path={RouteInfo.register.relativePath} handler={MKPlaceHolder}/>
             <Route name={RouteInfo.login.name} path={RouteInfo.login.relativePath} handler={MKPlaceHolder}/>
           </Route>
           <Route name={RouteInfo.shop.name} path={RouteInfo.shop.relativePath} handler={MKPlaceHolder}>
@@ -31,26 +31,21 @@ var MyKoop = React.createClass({
           <Route name={RouteInfo.options.name} path={RouteInfo.options.relativePath} handler={MKPlaceHolder}/>
           <Route name={RouteInfo.stats.name} path={RouteInfo.stats.relativePath} handler={MKPlaceHolder}/>
           <Route name={RouteInfo.transaction.name} path={RouteInfo.transaction.relativePath} handler={MKPlaceHolder}>
-            <Route name={RouteInfo.transaction_list.name} path={RouteInfo.transaction_list.relativePath} handler={MKPlaceHolder}/>
           </Route>
           <Route name={RouteInfo.events.name} path={RouteInfo.events.relativePath} handler={MKPlaceHolder}>
-            <Route name={RouteInfo.events_list.name} path={RouteInfo.events_list.relativePath} handler={MKPlaceHolder}/>
             <Route name={RouteInfo.events_pos.name} path={RouteInfo.events_pos.relativePath} handler={MKPlaceHolder}/>
             <Route name={RouteInfo.events_report.name} path={RouteInfo.events_report.relativePath} handler={MKPlaceHolder}/>
             <Route name={RouteInfo.events_signup.name} path={RouteInfo.events_signup.relativePath} handler={MKPlaceHolder}/>
           </Route>
           <Route name={RouteInfo.items.name} path={RouteInfo.items.relativePath} handler={MKPlaceHolder}>
-            <Route name={RouteInfo.items_list.name} path={RouteInfo.items_list.relativePath} handler={MKPlaceHolder}/>
             <Route name={RouteInfo.items_itemsbelowthreshold.name} path={RouteInfo.items_itemsbelowthreshold.relativePath} handler={MKPlaceHolder}/>
             <Route name={RouteInfo.items_nextorder.name} path={RouteInfo.items_nextorder.relativePath} handler={MKPlaceHolder}/>
           </Route>
           <Route name={RouteInfo.mailing.name} path={RouteInfo.mailing.relativePath} handler={MKPlaceHolder}>
-            <Route name={RouteInfo.mailing_mailinglists.name} path={RouteInfo.mailing_mailinglists.relativePath} handler={MKPlaceHolder}/>
             <Route name={RouteInfo.mailing_send.name} path={RouteInfo.mailing_send.relativePath} handler={MKPlaceHolder}/>
             <Route name={RouteInfo.mailing_subscribe.name} path={RouteInfo.mailing_subscribe.relativePath} handler={MKPlaceHolder}/>
           </Route>
           <Route name={RouteInfo.members.name} path={RouteInfo.members.relativePath} handler={MKPlaceHolder}>
-            <Route name={RouteInfo.members_list.name} path={RouteInfo.members_list.relativePath} handler={MKPlaceHolder}/>
             <Route name={RouteInfo.members_permissions.name} path={RouteInfo.members_permissions.relativePath} handler={MKPlaceHolder}/>
             <Route name={RouteInfo.volunteer_availability.name} path={RouteInfo.volunteer_availability.relativePath} handler={MKPlaceHolder}/>
             <Route name={RouteInfo.volunteer_schedule.name} path={RouteInfo.volunteer_schedule.relativePath} handler={MKPlaceHolder}/>
