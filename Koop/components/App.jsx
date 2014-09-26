7f9a238d689e2f29388ebaedd31c6f09e5c2c3ce
Cellule <mike.ferris@hotmail.com>
2014-09-26 11:26:36 -0400

    Seperate dev nav bar from app

--- Koop/components/App.jsx
+++ Koop/components/App.jsx
@@ -1,33 +1,17 @@
-var React = require("react/addons");
+var React = require("react");
 
-var MKNavigationBar = require("components/NavigationBar");
+var MKDevNavBar = require("components/DevNavBar");
 var BSGrid = require("react-bootstrap/Grid");
 var BSRow = require("react-bootstrap/Row");
-var BSButton = require("react-bootstrap/Button");
 
-var RouteInformation = require("routeInformation");
 
 var App = React.createClass({
-  getInitialState: function () {
-    return {
-      isDevBarHidden:true
-    };
-  },
-  toggleDevBarDisplay : function(){
-    this.setState({isDevBarHidden: !this.state.isDevBarHidden});
-  },
+
   render: function() {
-    var showHideButtonName = this.state.isDevBarHidden ? "Show Dev NavBar" : "Hide Dev NavBar";
-    var classes = React.addons.classSet({
-      hidden:this.state.isDevBarHidden
-    });
     return (
       <BSGrid fluid>
         <BSRow>
-          <BSButton onClick={this.toggleDevBarDisplay} bsSize="small">{showHideButtonName}</BSButton>
-          <div className={classes}>
-            <MKNavigationBar contentUrl={RouteInformation.devNavBar.fullPath}/>
-          </div>
+          <MKDevNavBar hide/>
         </BSRow>
         <BSRow style={{border:"1px solid black"}}>
             {this.props.activeRouteHandler()}
