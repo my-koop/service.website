efc1a364bc3e2ec5c4ba64343c9a106bf2c0c8fc
Cellule <mike.ferris@hotmail.com>
2014-09-27 12:45:20 -0400

    Put the login modal in a seperate component Homepage uses modal trigger instead of overlaymixin

--- Koop/components/Homepage.jsx
+++ Koop/components/Homepage.jsx
@@ -1,32 +1,20 @@
 ﻿var React = require("react");
 var Link = require("react-router").Link;
 var BSCol = require("react-bootstrap/Col");
-var BSOverlayMixin = require("react-bootstrap/OverlayMixin");
-var MKLoginBox = require("components/LoginBox");
 var BSButton = require("react-bootstrap/Button");
 var BSModal = require("react-bootstrap/Modal");
+var BSModalTrigger = require("react-bootstrap/ModalTrigger");
+
+var MKLoginModal = require("components/LoginModal");
 
 var Homepage = React.createClass({
-
-  mixins: [BSOverlayMixin],
-
-  getInitialState: function(){
-    return {
-      showLogin: false,
-      loginState: {}
-    };
-  },
-
-  handleLoginShow: function(){
-    this.setState({
-      showLogin: !this.state.showLogin
-    });
-  },
 
   render: function() {
     return (
       <BSCol md={6} mdOffset={3}>
-        <BSButton onClick={this.handleLoginShow} >Login</BSButton>
+        <BSModalTrigger modal={<MKLoginModal />} >
+          <BSButton >Login</BSButton>
+        </BSModalTrigger>
         <div>
           Coop Bécik est une coopérative de réparation qui a pour mission de rendre accessible l’utilisation du vélo comme mode de transport aux citoyen-ne-s et étudiant-e-s de Montréal.
         </div>
@@ -35,29 +23,8 @@ var Homepage = React.createClass({
         </div>
       </BSCol>
     );
-  },
-
-  renderOverlay: function () {
-    if(!this.state.showLogin){
-      return <span/>;
-    }
-    var self = this;
-    var saveLoginState = function(state){
-      self.setState({
-        loginState: state
-      });
-    };
-    return (
-      <BSModal title="Please Sign In" onRequestHide={this.handleLoginShow}>
-        <div className="modal-body">
-          <MKLoginBox state={this.state.loginState} saveStateFunction={saveLoginState}/>
-        </div>
-        <div className="modal-footer">
-          <BSButton onClick={this.handleLoginShow}>Close</BSButton>
-        </div>
-      </BSModal>
-    );
   }
+
 });
 
 module.exports = Homepage;
