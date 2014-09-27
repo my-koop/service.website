dcb8970d0af5ad84136705ddb4b573d1a40a0968
Cellule <mike.ferris@hotmail.com>
2014-09-27 14:13:13 -0400

    basic user account update page need to add more field in profile tab

---
+++ Koop/components/PasswordChangeForm.jsx
@@ -0,0 +1,31 @@
+﻿var React = require("react");
+var BSCol = require("react-bootstrap/Col");
+var BSInput = require("react-bootstrap/Input");
+var BSButton = require("react-bootstrap/Button");
+
+var PasswordChangeForm = React.createClass({
+
+  onSubmit: function(e){
+    return false;
+  },
+
+  render: function() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <BSInput 
+            type="password" 
+            label="Old Password" 
+          />
+          <BSInput 
+            type="password" 
+            label="New Password" 
+          />
+          <BSInput block type="submit" bsStyle="success" bsSize="large" value="Update Password" />
+        </form>
+      </div>
+    );
+  }
+});
+
+module.exports = PasswordChangeForm;
