a64544a17f008c48e12fcb5fc0296ef400103d9e
Cellule <mike.ferris@hotmail.com>
2014-09-27 23:54:02 -0400

    Fix code convention in app.ts and serve static files only in dev

--- Koop/app.ts
+++ Koop/app.ts
@@ -20,20 +20,20 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(methodOverride());
 app.use(session({
-    resave: true,
-    saveUninitialized: true,
-    secret: 'uwotm8'
+  resave: true,
+  saveUninitialized: true,
+  secret: 'uwotm8'
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-    app.use(errorHandler());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(errorHandler());
 }
 
 http.createServer(app).listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port'));
 });
