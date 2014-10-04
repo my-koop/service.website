436d8cab382ac720a33a442f67ce08931829ab86
ad <stepanian.adam@live.com>
2014-10-04 16:11:55 -0400

    Integrated tablesorter in EventsList.jsx, also added placeholder Modals using Abstract Modal

--- Koop/components/EventsList.jsx
+++ Koop/components/EventsList.jsx
@@ -1,35 +1,211 @@
-var React = require("react");
-var PropTypes = React.PropTypes;
-var FilteredList = require("components/FilterableItemList");
-var BSButton = require("react-bootstrap/Button");
+var React           = require("react");
+var PropTypes       = React.PropTypes;
+var BSButton        = require("react-bootstrap/Button");
+var BSCol           = require("react-bootstrap/Col");
+var BSModalTrigger  = require("react-bootstrap/ModalTrigger");
 
-var editButton = <BSButton>Edit</BSButton>;
+var MKTableSorter   = require("components/TableSorter");
+var MKListModButtons= require("components/ListModButtons");
+var MKItemEditModal = require("components/ItemEditModal");
+var FormInputFactory= require("components/FormInputFactory");
+var MKAbstractModal = require("components/AbstractModal");
 
-var headers = ["ID","Name","Date","Fonction"];
-var data = [
-    ["1","Daily Operation","2013/10/01",editButton],
-    ["2","Mobile Clinic","2013/08/27",editButton],
-    ["3","Mobile Clinic","2013/08/19",editButton],
-    ["4","Daily Operation","2013/09/22",editButton],
-    ["5","Daily Operation","2013/09/25",editButton],
-    ["6","Daily Operation","2013/09/28",editButton]
-    ];
-var filters = [
-    {
-      "properties": {
-        "name": "search",
-        "placeholder": "search",
-        "label": "Search by ID"
-      },
-     "type": "text"
-    }
+var EventBoxInputs  = [
+  {
+    "properties": {
+      "name": "eventType",
+      "label": "Select event type",
+      "options": [
+        {
+          "name": "eventType_Daily",
+          "value": "Daily Operation"
+        },
+        {
+          "name": "eventType_MobileClinic",
+          "value": "Mobile Clinic"
+        }
+      ]
+    },
+    "type": "select"
+  },
+  {
+    "properties": {
+      "name": "date",
+      "label": "Date",
+      "placeholder": "YYYY/MM/DD"
+    },
+    "type": "text"
+  }, 
+  {
+    "properties": {
+      "name": "startTime",
+      "placeholder": "HH:MM",
+      "label": "Enter start time"
+    },
+    "type": "text"
+  }
 ];
-var EventsList = React.createClass({
-    render: function(){
+
+var EventBox = React.createClass({
+  propTypes: {
+    inputs: PropTypes.array.isRequired
+  },
+
+  render: function(){
+    inputElements = this.props.inputs.map(function(input,key){
       return (
-        <FilteredList filterOptions={filters} headers={headers} data={data} />
+        FormInputFactory(input.type,input.properties,key)
       );
+    });
+    return (
+      <div>
+        {inputElements}
+      </div>
+    );
+  }
+});
+
+var modalBody = (
+  <div>
+      <EventBox inputs={EventBoxInputs} />
+      <BSButton bsStyle="primary" type="submit">Confirm</BSButton>
+  </div>
+);
+
+var TriggerEventModal = function(buttonName){
+  return (
+    <div>
+      <BSModalTrigger modal={
+        <MKAbstractModal 
+          title="Event informations" 
+          modalBody={modalBody} 
+          useCloseButtonFooter={true} 
+        />
+        } 
+      >
+        <BSButton>{buttonName}</BSButton>
+      </BSModalTrigger>
+    </div>
+  );
+};   
+
+var actionsGenerator = function(item){
+  return [
+    {
+      icon: "minus",
+      warningMessage: "Are you sure?",
+      tooltip: {
+        text: "Delete",
+        overlayProps: {
+          placement: "top"
+        }
+      }
+    },
+    {
+      icon: "edit",
+      tooltip: {
+        text: "Edit Item",
+        overlayProps: {
+          placement: "right"
+        }
+      },
+      customWrapper: function(component, iBtn){
+        return (
+          <BSModalTrigger
+            key={iBtn}
+            modal={
+              <MKAbstractModal 
+                title="Enter your availability" 
+                modalBody={modalBody} 
+                useCloseButtonFooter={true} 
+              /> }
+          >
+            {component}
+          </BSModalTrigger>
+        );
+      }
+
+    },
+  ];
+}
+
+var EventsList = React.createClass({
+  getInitialState: function(){
+    return {
+      items:  [
+        {
+          "id": 1,
+          "col1": "Daily Operation",
+          "col2": "2013/10/01",
+          "col3": "12:00"
+        },
+        {
+          "id": 2,
+          "col1": "Daily Operation",
+          "col2": "2013/09/25",
+          "col3": "16:00"
+        },
+        {
+          "id": 3,
+          "col1": "Mobile Clinic",
+          "col2": "2013/09/24",
+          "col3": "18:00"
+        },
+        {
+          "id": 4,
+          "col1": "Daily Operation",
+          "col2": "2013/09/24",
+          "col3": "16:00"
+        },
+        {
+          "id": "5",
+          "col1": "Daily Operation",
+          "col2": "2013/09/22",
+          "col3": "18:00"
+        }
+      ]
     }
+  },
+  render: function(){
+    var CONFIG = {
+      columns: {
+        id: { name: "ID" },
+        col1: { name: "Event Type" },
+        col2: { name: "Date" },
+        col3: { name: "Start Time" },
+        editCol: {
+          name: "Actions",
+          disableSort: true,
+          disableFilter: true,
+          cellGenerator: function(item){
+            var self = this;
+            return (
+              <MKListModButtons
+                defaultTooltipDelay={500}
+                buttons={actionsGenerator.call(this,item)}
+              />
+            );
+          }
+        }
+      }
+    };
+    return (
+      <BSCol md={12}>
+      <div>
+        <MKTableSorter
+          config={CONFIG}
+          items={this.state.items}
+          headerRepeat={8}
+          striped
+          bordered
+          condensed
+          hover
+        />
+        {TriggerEventModal("Add New")}
+      </div>
+    </BSCol>
+    );
+  }
 });
 
 module.exports = EventsList;
