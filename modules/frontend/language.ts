87c165b3ed62ad681693bc479eeccb494259dcae
Mathieu M-Gosselin <mathieumg@gmail.com>
2014-10-22 14:25:17 -0400

    Forgot to remove commented-out line.

--- modules/frontend/language.ts
+++ modules/frontend/language.ts
@@ -1,6 +1,5 @@
 ﻿///<reference path="../../typings/i18next/i18next.d.ts" />
 import i18n = require("i18next");
-//var i18n = require("i18next");
 var translations = require("dynamic-metadata").translations;
 
 i18n.init({
