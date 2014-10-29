12e070df000932ab80c1901b64056deac12d8ad5
Cellule <mike.ferris@hotmail.com>
2014-10-29 14:33:29 -0400

    hotfix superagent get requests

--- modules/frontend/ajax.ts
+++ modules/frontend/ajax.ts
@@ -13,11 +13,21 @@ export interface AjaxCallback {
 
 export function request(params: AjaxParams, callback: AjaxCallback) {
   var method = params.method ? params.method.toUpperCase() : "GET";
-  var data = params.data || {};
+  var data = method === "GET" ?
+    {
+      query: params.data,
+      send: <any>{}
+    }
+    :
+    {
+      query: <any>{},
+      send: params.data
+    };
 
   //TODO: Do we want to wrap the response a little? Consider 404 an error,
   // only send the reponse body, etc.
   superagent(method, params.endpoint)
-    .send(data)
+    .query(data.query)
+    .send(data.send)
     .end(callback);
 }
